65a789857e4696d9897b6a3a6bdf175d0f561e76
getSohelRana <getsohelinfo@gmail.com>
2025-09-19 11:31:08 +0600

    check react code

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ function App() {
   
   return (
     <div>
+      <Student></Student>
+    </div>
+  )
+}
+
+function Student () {
+  return(
+    <div>
       <h1 class="text-3xl font-bold underline text-red-600 text-center">
         Hello world!
       </h1>
