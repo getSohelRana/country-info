9d8cd52b30868d0afb036917d6aaf475f2ef924d
getSohelRana <getsohelinfo@gmail.com>
2025-09-19 11:11:32 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline text-red-600 text-center">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
